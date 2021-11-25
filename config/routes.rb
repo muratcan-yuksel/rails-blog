561fcf37864c822478de64f932e279f8073d8ece
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-11-25 16:03:05 +0300

    -

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get "articles", to:"articles#index"
  get "about", to: "about#index"
- 
+ root "articles#index"
+
+resources :articles 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
