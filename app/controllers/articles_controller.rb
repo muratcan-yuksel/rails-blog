52d5b6dc54388f0df51a6812fd1e6045a664b57a
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-11-30 03:23:14 +0300

    .

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,22 +3,21 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-def show
-  @article= Article.find(params[:id])
-end
-
-def new
-  @article = Article.new 
-end
-
-def create
-  @article = Article.new(title:"...", body:"...")
-  if @article.save
-    redirect_to @article
-  else
-    render :new
+  def show
+    @article = Article.find(params[:id])
   end
-end
 
+  def new
+    @article = Article.new
+  end
 
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
 end
